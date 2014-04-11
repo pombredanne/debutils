c4bd42ce0c711eed5ea45f9919c329386fb73e9b
Michael Greene <a.minor.threat@gmail.com>
2014-04-11 13:21:26 -0700

    Changed name to Debutils, added PyPI classifiers

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_reqs = parse_requirements('requirements-test.txt')
 
 ##TODO: fill in blank fields
 setup(
-    name             = 'PyDeb',
+    name             = 'Debutils',
     version          = pydeb._author.__version__,
     description      = 'Python library for doing things with Debian packages and repositories',
     long_description = long_desc,
@@ -22,7 +22,17 @@ setup(
     url              = "",
     download_url     = "",
 
-    classifiers = [],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Software Distribution',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
     keywords    = [],
 
     install_requires = [str(ir.req) for ir in reqs],
