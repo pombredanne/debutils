9efad0b3951ba10538f9abdc7dbee70a4b14fba1
Michael Greene <a.minor.threat@gmail.com>
2014-04-16 18:46:00 -0700

    added lots of stuff for nicer pretty printing, cleaned up memory use somewhat

--- debutils/Deb/debfile.py
+++ debutils/Deb/debfile.py
@@ -4,19 +4,79 @@ Pythonic interface for accessing .deb files
 """
 
 import tarfile
-import arfile
-import io
+import collections
+import collections.abc
+import time
 
-class DebMetadata:
+from debutils.Deb.arfile import ArFile
+from debutils.Deb.util import modestr
+
+
+class DebView(collections.abc.ItemsView):
+    def __str__(self):
+        s = ""
+        for i in self:
+            s += "{mode:10s} {uid}/{gid} {size:>9d} {date:16} {member}\n".format(
+                mode=modestr(i[1]["mode"]),
+                uid=i[1]["uname"],
+                gid=i[1]["gname"],
+                size=i[1]["size"],
+                date=time.strftime("%Y-%m-%d %H:%M:%S", i[1]["mtime"]),
+                member=i[0]
+            )
+        return s
+
+    def __iter__(self):
+        for key in self._mapping:
+            yield self._mapping[key]
+
+
+class MetaDeb(collections.OrderedDict):
+    def __init__(self, debtar):
+        super(MetaDeb, self).__init__()
+
+        with tarfile.open(fileobj=debtar, mode='r:gz') as tar:
+            for ti in tar:
+                member = {}
+                member["name"] = ti.name
+                member["size"] = ti.size
+                member["mtime"] = time.localtime(ti.mtime)
+                member["mode"] = ti.mode
+                member["type"] = ti.type
+                member["uid"] = ti.uid
+                member["gid"] = ti.gid
+                member["uname"] = ti.uname
+                member["gname"] = ti.gname
+
+
+                self[ti.name] = member
+
+    def items(self):
+        return DebView(self)
+
+    def __str__(self):
+        s = ""
+        for i in self.items():
+            s += str(i) + "\n"
+        return s
+
+
+class DebMetadata(MetaDeb):
     def __init__(self, debcontrol):
-        self.t = tarfile.open(fileobj=debcontrol, mode='r|gz')
-        self.files = self.t.getmembers()
+        super(DebMetadata, self).__init__(debcontrol)
+
+        debcontrol.seek(0)
+        with tarfile.open(fileobj=debcontrol, mode='r:gz') as tar:
+            for cfile in self.keys():
+                file = tar.extractfile(cfile)
+
+                if file is not None:
+                    self[cfile]["contents"] = file.read()
 
 
-class DebContents:
+class DebContents(MetaDeb):
     def __init__(self, debcontents):
-        self.t = tarfile.open(fileobj=debcontents, mode='r|gz')
-        self.files = self.t.getmembers()
+        super(DebContents, self).__init__(debcontents)
 
 
 class DebFile:
@@ -27,11 +87,24 @@ class DebFile:
         """
         :param debfile: file path, file handle, or byte array contents of a .deb file
         """
-        self._ar = arfile.ArFile(debfile)
-        self.control = DebMetadata(io.BytesIO(self._ar.files["control.tar.gz"]["data"]))
-        self.data = DebContents(io.BytesIO(self._ar.files["data.tar.gz"]["data"]))
+        self._ar = ArFile(debfile)
+
+        self._control = DebMetadata(self._ar.files["control.tar.gz"]["data"])
+        self._contents = DebContents(self._ar.files["data.tar.gz"]["data"])
+        pass
+
+    def contents(self):
+        return self._contents.items()
 
 if __name__ == "__main__":
-    kiosk = DebFile("/Users/magreene/Dev/kiosk/packages/final/kiosk_3.05_all.deb")
-    kiosk_panel_timer = DebFile("/Users/magreene/Dev/kiosk/packages/final/kiosk-panel-timer_3.04-2_i386.deb")
-    print("")
+    example = DebFile("tests/testdata/example_1.0-1_all.deb")
+
+    print("print(example.contents())")
+    print(example.contents())
+    print(type(example.contents()))
+    print()
+
+    print("for i in example.contents()")
+    for i in example.contents():
+        print(i)
+        print(type(i))
