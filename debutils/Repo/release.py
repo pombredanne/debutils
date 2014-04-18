a4d7b5b962d578bb583f3d1a02c4197c4ff7188c
Michael Greene <a.minor.threat@gmail.com>
2014-04-17 20:51:24 -0700

    added new subpackage Repo to debutils

---
+++ debutils/Repo/release.py
@@ -0,0 +1,11 @@
+""" release.py
+
+Generate and sign Release files
+"""
+
+from .._parsers.releasefile import ReleaseFile, ReleaseGPGFile
+
+
+class Release:
+    ##TODO: design the API for this
+    pass
