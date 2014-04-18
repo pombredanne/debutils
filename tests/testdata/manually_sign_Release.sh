3b33edda8dc02f64f5b44d3b79bd306aa6e64e6e
Michael Greene <a.minor.threat@gmail.com>
2014-04-18 14:40:13 -0700

    short script for manually signing Release to generate Release.gpg

---
+++ tests/testdata/manually_sign_Release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shortcut to use the debutils.gpg and debutils.sec.gpg keyring pair only
+GPG="`which gpg` --no-default-keyring --keyring ./debutils.gpg --secret-keyring ./debutils.sec.gpg"
+
+# ensure we're in tests/testdata
+cd "$(dirname "$0")"
+
+# get the signature of the Debutils key from the keyring pair
+KEY_SIG=$( $GPG --list-sigs | awk '/^sig/ {print $3}' )
+
+# generate detached signature Release.gpg
+$GPG --default-key $KEY_SIG --armor --detach-sign --sign --output Release.gpg Release
