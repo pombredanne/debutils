7f71eafda43bfbe857fa00b5ceafb46f46f3f24c
Michael Greene <a.minor.threat@gmail.com>
2014-04-17 20:47:22 -0700

    changed import path; cleaned up formatting slightly

--- tests/test_arFile.py
+++ tests/test_arFile.py
@@ -1,28 +1,34 @@
 import pytest
-from debutils.Deb.arfile import ArFile, ArchiveError
+
+from debutils._parsers.arfile import ArFile
+
+
+try:
+    e = FileNotFoundError
+except NameError:
+    e = IOError
 
 @pytest.fixture(scope="module",
-                params=["tests/testdata/example_1.0-1_all.deb",
-                open("tests/testdata/example_1.0-1_all.deb", 'rb'),
-                open("tests/testdata/example_1.0-1_all.deb", 'rb').read()])
+                params=[
+                    "tests/testdata/example_1.0-1_all.deb",
+                    "tests/testdata/sym_example_1.0-1_all.deb",
+                    open("tests/testdata/example_1.0-1_all.deb", 'rb'),
+                    open("tests/testdata/example_1.0-1_all.deb", 'rb').read()
+                ])
 def ar(request):
     return ArFile(request.param)
 
 
 class TestArFile:
     def test_errors(self):
-        with pytest.raises(ArchiveError):
+        with pytest.raises(NotImplementedError):
             ArFile(None)
 
-        try:
-            # only works in Python 3.3+
-            with pytest.raises(FileNotFoundError):
-                ArFile("/this/path/does/not/exist")
+        with pytest.raises(e):
+            ArFile("/this/path/does/not/exist")
 
-        except NameError:
-            # on Python <=3.2, this should raise an IOError
-            with pytest.raises(IOError):
-                ArFile("/this/path/does/not/exist")
 
     def test_ar(self, ar):
-        assert list(ar.files.keys()) == ["debian-binary", "control.tar.gz", "data.tar.gz"]
+        assert list(ar.files.keys()) == ["debian-binary",
+                                         "control.tar.gz",
+                                         "data.tar.gz"]
