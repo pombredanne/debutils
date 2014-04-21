f24c93d2ee994de97c1f7e15b0eacaeec5d3d361
Michael Greene <a.minor.threat@gmail.com>
2014-04-21 15:48:16 -0700

    moved file load tests to new test harness test_FileLoader

--- tests/test_arFile.py
+++ tests/test_arFile.py
@@ -2,31 +2,15 @@ import pytest
 
 from debutils._parsers.arfile import ArFile
 
-
-try:
-    e = FileNotFoundError
-except NameError:
-    e = IOError
-
-@pytest.fixture(scope="module",
-                params=[
-                    "tests/testdata/example_1.0-1_all.deb",
-                    "tests/testdata/sym_example_1.0-1_all.deb",
-                    open("tests/testdata/example_1.0-1_all.deb", 'rb'),
-                    open("tests/testdata/example_1.0-1_all.deb", 'rb').read()
-                ])
+@pytest.fixture(scope="module")
 def ar(request):
-    return ArFile(request.param)
+    return ArFile("tests/testdata/example_1.0-1_all.deb")
 
 
 class TestArFile:
     def test_errors(self):
         with pytest.raises(NotImplementedError):
             ArFile(None)
-
-        with pytest.raises(e):
-            ArFile("/this/path/does/not/exist")
-
 
     def test_ar(self, ar):
         assert list(ar.files.keys()) == ["debian-binary",
