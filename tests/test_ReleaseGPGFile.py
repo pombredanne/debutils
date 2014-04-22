057e6cef95e4313050bc03a64bd2ddb55ee7462d
Michael Greene <a.minor.threat@gmail.com>
2014-04-21 17:54:39 -0700

    updated harness to test parsing a Release.gpg from the web; added actual print test

--- tests/test_ReleaseGPGFile.py
+++ tests/test_ReleaseGPGFile.py
@@ -1,14 +1,21 @@
 import pytest
 from debutils._parsers.releasefile import ReleaseGPGFile
 
-@pytest.fixture(scope="module")
-def release_gpg():
-    return ReleaseGPGFile("tests/testdata/Release.gpg")
+@pytest.fixture(scope="module",
+                params=[
+                    "tests/testdata/Release.gpg",
+                    "http://us.archive.ubuntu.com/ubuntu/dists/precise/Release.gpg"
+                ])
+def release_gpg(request):
+    return ReleaseGPGFile(request.param)
 
 
 class TestReleaseGPGFile:
     def test_parse(self, release_gpg):
-        pass
+        assert release_gpg.crc == release_gpg.crc24()
 
-    def test_print(self, release_gpg):
-        pass
+    def test_print(self, release_gpg, capsys):
+        print(release_gpg)
+        out, _ = capsys.readouterr()
+
+        assert out == release_gpg.bytes.decode() + '\n'
