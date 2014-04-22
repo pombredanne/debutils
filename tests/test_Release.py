76793850947e4547abf52b654d2016e6e3da0f08
Michael Greene <a.minor.threat@gmail.com>
2014-04-21 19:26:32 -0700

    basic skeleton test harness for testing debutils.Repo.Release

---
+++ tests/test_Release.py
@@ -0,0 +1,31 @@
+import pytest
+
+from debutils.Repo import Release
+
+
+@pytest.fixture(scope="module",
+                params=[
+                    ("tests/testdata/Release", "tests/testdata/Release.gpg"),
+                    ("http://us.archive.ubuntu.com/ubuntu/dists/precise/Release",
+                     "http://us.archive.ubuntu.com/ubuntu/dists/precise/Release.gpg"),
+                    ("http://http.debian.net/debian/dists/sid/Release",
+                     "http://http.debian.net/debian/dists/sid/Release.gpg")
+                ])
+def release_verify(request):
+    return Release(request.param[0], request.param[1])
+
+@pytest.fixture(scope="module")
+def release_sign(request):
+    return Release("tests/testdata/Release")
+
+
+class TestRelease:
+    def test_verify_release(self, release_verify):
+        with pytest.raises(NotImplementedError):
+            ##TODO: write this test once sign_with is implemented
+            release_verify.verify()
+
+    def test_sign_release(self, release_sign):
+        with pytest.raises(NotImplementedError):
+            ##TODO: write this test once sign_with is implemented
+            release_sign.sign_with(None)
