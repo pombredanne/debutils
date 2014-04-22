db6eb2ca2adc6bb3b9d673fc046ca1ba16df68cd
Michael Greene <a.minor.threat@gmail.com>
2014-04-21 19:30:30 -0700

    started fleshing out the API for debutils.Repo.Release. PyGPGME was determined to not be a good fit, unfortunately. Most of the other Python GPG wrappers seem to basically do the same thing - shell out to the gpg binary for all tasks, which is not acceptable for this project.

--- debutils/Repo/release.py
+++ debutils/Repo/release.py
@@ -3,9 +3,27 @@
 Generate and sign Release files
 """
 
+import io
+
 from .._parsers.releasefile import ReleaseFile, ReleaseGPGFile
+from .._parsers.fileloader import FileLoader
+
+
+class ReleaseError(Exception):
+    pass
 
 
 class Release:
-    ##TODO: design the API for this
-    pass
+    def __init__(self, rfile=None, sigfile=None):
+        self.release = ReleaseFile(rfile)
+        self.sig = ReleaseGPGFile(sigfile)
+
+    def verify(self):
+        if None in [self.release, self.sig]:
+            raise ReleaseError("Release.verify requires both a Release and a Release signature")
+
+        raise NotImplementedError()
+
+    def sign_with(self, secret_key):
+        ##TODO: implement sign_with
+        raise NotImplementedError()
