93498598200f7e6ddecf25526c6c7dd3a0a56b1d
Michael Greene <a.minor.threat@gmail.com>
2014-04-21 17:55:43 -0700

    added crc24 computation, fixed a few PEP8 style complaints, and implemented __str__

--- debutils/_parsers/releasefile.py
+++ debutils/_parsers/releasefile.py
@@ -9,6 +9,7 @@ import time
 import base64
 
 from .fileloader import FileLoader
+from ..util import bytes_to_int, int_to_bytes
 
 
 class ReleaseFile(FileLoader):
@@ -177,28 +178,74 @@ class ReleaseGPGFile(FileLoader):
         r'^(.*)\n'\
         r'^-----END PGP SIGNATURE-----$\n'
 
+    crc24_init = 0xB704CE
+    crc24_poly = 0x1864CFB
+
     def __init__(self, releasesig):
         self.headers = collections.OrderedDict()
-        self.sig     = None
-        self.crc  = None
+        self.sig = None
+        self.crc = None
 
         super(ReleaseGPGFile, self).__init__(releasesig)
 
     def parse(self):
         # parsing/decoding using the RFC 4880 section on "Forming ASCII Armor"
         # https://tools.ietf.org/html/rfc4880#section-6.2
-        k = re.split(self.gpg_sig_format, self.bytes.decode(), flags=re.MULTILINE|re.DOTALL)[1:-1]
+        k = re.split(self.gpg_sig_format, self.bytes.decode(), flags=re.MULTILINE | re.DOTALL)[1:-1]
 
         # parse header field(s)
         h = [ h for h in re.split(r'^([^:]*): (.*)$\n?', k[0], flags=re.MULTILINE) if h != '' ]
         for key, val in [ (h[i], h[i+1]) for i in range(0, len(h), 2) ]:
             self.headers[key] = val
 
+        ##TODO: dump fields in sig per RFC 4880
         self.sig = base64.b64decode(k[1].replace('\n', '').encode())
-        self.crc = base64.b64decode(k[2].encode())
+        self.crc = bytes_to_int(base64.b64decode(k[2].encode()))
 
-    # def __str__(self):
-    #     return \
-    #         "-----BEGIN PGP SIGNATURE-----\n"\
-    #
-    #         "-----END PGP SIGNATURE-----\n"
+        ## verify CRC
+        if self.crc != self.crc24():
+            raise Exception("Bad CRC")
+
+    def crc24(self):
+        # CRC24 computation, as described in the RFC 4880 section on Radix-64 Conversions
+        #
+        # The checksum is a 24-bit Cyclic Redundancy Check (CRC) converted to
+        # four characters of radix-64 encoding by the same MIME base64
+        # transformation, preceded by an equal sign (=).  The CRC is computed
+        # by using the generator 0x864CFB and an initialization of 0xB704CE.
+        # The accumulation is done on the data before it is converted to
+        # radix-64, rather than on the converted data.
+        if self.sig is None:
+            return None
+
+        crc = self.crc24_init
+        sig = [ ord(i) for i in self.sig ] if type(self.sig) is str else self.sig
+        for loc in range(0, len(self.sig)):
+            crc ^= sig[loc] << 16
+
+            for i in range(0, 8):
+                crc <<= 1
+                if crc & 0x1000000:
+                    crc ^= self.crc24_poly
+
+        return crc & 0xFFFFFF
+
+    def __str__(self):
+        headers = ""
+        for key, val in self.headers.items():
+            headers += "{key}: {val}\n".format(key=key, val=val)
+
+        # base64-encode self.sig, then insert a newline every 64th character
+        payload = base64.b64encode(self.sig).decode()
+        payload = '\n'.join(payload[i:i+64] for i in range(0, len(payload), 64))
+
+        return \
+            "-----BEGIN PGP SIGNATURE-----\n"\
+            "{headers}\n"\
+            "{sig}\n"\
+            "={crc}\n"\
+            "-----END PGP SIGNATURE-----\n".format(
+                headers=headers,
+                sig=payload,
+                crc=base64.b64encode(int_to_bytes(self.crc)).decode(),
+            )
