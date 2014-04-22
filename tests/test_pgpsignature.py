65b954bf9497389684bb0bbb2d824c97a4fac242
Michael Greene <a.minor.threat@gmail.com>
2014-04-22 14:14:34 -0700

    since everything for ReleaseSignature is being done by PGPSignature, it made more sense to run these tests on that class

--- tests/test_pgpsignature.py
+++ tests/test_pgpsignature.py
@@ -1,4 +1,28 @@
 import pytest
-from debutils._parsers.pgp import PGPSignature
+import pgpdump
+from debutils.pgp.signature import PGPSignature
 
-##TODO: implement PGP signature packet tests
+@pytest.fixture(scope="module",
+                params=[
+                    "tests/testdata/Release.gpg",
+                    "http://us.archive.ubuntu.com/ubuntu/dists/precise/Release.gpg",
+                    "http://http.debian.net/debian/dists/sid/Release.gpg"
+                ])
+def pgpsig(request):
+    return (PGPSignature(request.param), request.param)
+
+
+class TestPGPSignature:
+    def test_parse(self, pgpsig):
+        pgps = pgpsig[0]
+        pgps_path = pgpsig[1]
+
+        assert pgps.crc == pgps.crc24()
+
+    def test_print(self, pgpsig, capsys):
+        pgps = pgpsig[0]
+
+        print(pgps)
+        out, _ = capsys.readouterr()
+
+        assert out == pgps.bytes.decode() + '\n'
