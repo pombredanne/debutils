99dcde2dcc28e15e8e1098cf2951080a04a594e2
Michael Greene <a.minor.threat@gmail.com>
2014-04-22 02:42:54 -0700

    commented out tests because the components they are testing will be moving to a different class

--- tests/test_Release.py
+++ tests/test_Release.py
@@ -20,12 +20,13 @@ def release_sign(request):
 
 
 class TestRelease:
-    def test_verify_release(self, release_verify):
-        with pytest.raises(NotImplementedError):
-            ##TODO: write this test once sign_with is implemented
-            release_verify.verify()
-
-    def test_sign_release(self, release_sign):
-        with pytest.raises(NotImplementedError):
-            ##TODO: write this test once sign_with is implemented
-            release_sign.sign_with(None)
+    pass
+    # def test_verify_release(self, release_verify):
+    #     with pytest.raises(NotImplementedError):
+    #         ##TODO: write this test once sign_with is implemented
+    #         release_verify.verify()
+    #
+    # def test_sign_release(self, release_sign):
+    #     with pytest.raises(NotImplementedError):
+    #         ##TODO: write this test once sign_with is implemented
+    #         release_sign.sign_with(None)
