0e74b6e10e6d721d8aecd71fb0c5e6645b167c72
Michael Greene <a.minor.threat@gmail.com>
2014-04-22 14:33:56 -0700

    fixed regex

--- debutils/pgp/signature.py
+++ debutils/pgp/signature.py
@@ -10,47 +10,13 @@ import hashlib
 from .._parsers.fileloader import FileLoader
 from .util import bytes_to_int, int_to_bytes
 
-# from the Computing Signatures section of RFC 4880 (http://tools.ietf.org/html/rfc4880#section-5.2.4)
-#
-# All signatures are formed by producing a hash over the signature
-# data, and then using the resulting hash in the signature algorithm.
-#
-# For binary document signatures (type 0x00), the document data is
-# hashed directly.  For text document signatures (type 0x01), the
-# document is canonicalized by converting line endings to <CR><LF>,
-# and the resulting data is hashed.
-#
-# ...
-#
-# ...
-#
-# ...
-#
-# Once the data body is hashed, then a trailer is hashed.
-# (...) A V4 signature hashes the packet body
-# starting from its first field, the version number, through the end
-# of the hashed subpacket data.  Thus, the fields hashed are the
-# signature version, the signature type, the public-key algorithm, the
-# hash algorithm, the hashed subpacket length, and the hashed
-# subpacket body.
-#
-# V4 signatures also hash in a final trailer of six octets: the
-# version of the Signature packet, i.e., 0x04; 0xFF; and a four-octet,
-# big-endian number that is the length of the hashed data from the
-# Signature packet (note that this number does not include these final
-# six octets).
-#
-# After all this has been hashed in a single hash context, the
-# resulting hash field is used in the signature algorithm and placed
-# at the end of the Signature packet.
-
 
 class PGPSignature(FileLoader):
     pgp_signature_ascii_format = \
         r'^-----BEGIN PGP SIGNATURE-----$\n'\
         r'(.*)\n\n'\
         r'(.*)'\
-        r'^(.{3})\n'\
+        r'^(=.{4})\n'\
         r'^-----END PGP SIGNATURE-----$\n'
 
     crc24_init = 0xB704CE
@@ -83,9 +49,6 @@ class PGPSignature(FileLoader):
             raise Exception("Bad CRC")
 
         ##TODO: dump fields in signature_packet per RFC 4880, without using pgpdump
-
-    ##TODO: add signature generation function
-    ##TODO: add signature verification function
 
     def crc24(self):
         # CRC24 computation, as described in the RFC 4880 section on Radix-64 Conversions
